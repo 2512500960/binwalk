cda9d98893c4021971024321be4b522e2a8ec3ff
erlkonig <2512500960@qq.com>
2023-09-07 18:08:18 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -115,9 +115,10 @@ function install_cramfstools
 
 function install_ubireader
 {
-    git clone --quiet --depth 1 --branch "main" https://github.com/jrspruitt/ubi_reader
-    (cd ubi_reader && pip install ubi_reader)
-    $SUDO rm -rf ubi_reader
+    #git clone --quiet --depth 1 --branch "main" https://github.com/jrspruitt/ubi_reader
+    #(cd ubi_reader && pip install ubi_reader)
+    pip install ubi_reader
+    #$SUDO rm -rf ubi_reader
 }
 
 function install_pip_package
