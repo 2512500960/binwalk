a5216e33423b13fed25a7154ce3ee276b65fca1a
erlkonig <2512500960@qq.com>
2023-09-07 17:40:29 +0800

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -236,7 +236,7 @@ if [ $? -ne 0 ]
     echo "Package installation failed: $PKG_CANDIDATES"
     exit 1
 fi
-install_pip_package "setuptools matplotlib capstone pycryptodome gnupg tk"
+install_pip_package "setuptools matplotlib capstone pycryptodome gnupg tk poetry"
 install_sasquatch
 install_yaffshiv
 install_jefferson
